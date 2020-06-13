53e8bfe4b410f82109bf0bd557281b48e61c645c
Steve Thomas <stthomas4531@gmail.com>
2020-06-12 23:55:20 -0600

    Home Page Update

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,15 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
       return (
-          <div>
-              <h1>QUIZ ME</h1>
-
-              <div>
-                  <View style={{ flexDirection: "row" }}>
-                      <View style={styles.buttonStyle}>
-                          <Button>Take A Quiz</Button>
-                      </View>
-                      <View style={styles.buttonStyle}>
-                          <Button>Score</Button>
-                      </View>
-                  </View>
-              </div>
+          <div style={{
+              width: "50%", height: "100%", backgroundColor: "red",
+              margin: "0 auto", display: "flex", flexDirection: "column",
+              alignItems: "center", justifyContent: "center"
+          }}>
+              <h1>QuizMe</h1>
+              <h2>Funny Phrase</h2>
+              <input type="button" value="Play" style={{ width: "75%", height: "50px", margin: "10px" }} />
+              <input type="button" value="Scoreboard" style={{ width: "75%", height: "50px", margin: "10px" }} />
           </div>
       );
   }
