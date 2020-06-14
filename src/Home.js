453958b6113772aa8cb782f2046650c620cf4f58
Steve Thomas <stthomas4531@gmail.com>
2020-06-13 18:40:43 -0600

    fixed home page style

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,14 @@ class Home extends Component {
   render() {
       return (
           <div style={{
-              width: "50%", height: "100%", backgroundColor: "red",
+              width: "50%", height: "100%",
               margin: "0 auto", display: "flex", flexDirection: "column",
               alignItems: "center", justifyContent: "center"
           }}>
               <h1>QuizMe</h1>
               <h2>Funny Phrase</h2>
-              <input type="button" value="Play" style={{ width: "75%", height: "50px", margin: "10px" }} />
-              <input type="button" value="Scoreboard" style={{ width: "75%", height: "50px", margin: "10px" }} />
+              <input type="button" value="Play" class="nes-btn is-primary" style={{ width: "75%", height: "50px", margin: "10px" }} />
+              <input type="button" value="Score" class="nes-btn is-primary" style={{ width: "75%", height: "50px", margin: "10px" }} />
           </div>
       );
   }
