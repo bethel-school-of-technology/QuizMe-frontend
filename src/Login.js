40d58cffb63c61099c59f355723870347af02d2d
Steve Thomas <stthomas4531@gmail.com>
2020-06-18 19:07:22 -0600

    input width

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ class Login extends Component {
                 margin: "0 auto", display: "flex", flexDirection: "column",
                 alignItems: "center", justifyContent: "center"
             }}>
-                <input type="text" value="Username" className="nes-input is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="text" value="Username" className="nes-input is-primary" style={{ width: "90%", height: "50px", margin: "10px", textAlign: "center" }}/>  
                 <input type="password" value="********" className="nes-input is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
                 <input type="button" value="Login" className="nes-btn is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
 
