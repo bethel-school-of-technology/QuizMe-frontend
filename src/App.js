6b030fe20c1c7527a410c5861637fa96016d5e91
Steve Thomas <stthomas4531@gmail.com>
2020-06-18 18:33:42 -0600

    loginPage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Quiz from './Quiz';
 import SelectCategory from './SelectCategory';
 import './App.css';
 import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Switch>
           <Route path="/" component={Home} exact/>
+          <Route path="/Login" component={Login} exact/>
           <Route path="/SelectCategory" component={SelectCategory}/>
           <Route path="/Quiz/:category" render={(props) => {
             return <Quiz {...props}/>
---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+
+class Login extends Component {
+    render() {
+        return (
+            <div style={{
+                width: "50%", height: "100%",
+                margin: "0 auto", display: "flex", flexDirection: "column",
+                alignItems: "center", justifyContent: "center"
+            }}>
+                <input type="text" value="Username" className="nes-input is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="password" value="********" className="nes-input is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="button" value="Login" className="nes-btn is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+
+            </div>
+        )}}
+export default Login;
