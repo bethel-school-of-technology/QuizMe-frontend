060bcdf7b298f65ab87a67227741e44e23728040
Sean Combs <seanmxd@gmail.com>
2020-06-23 21:05:09 -0400

    Fixed inputs

--- src/Login.js
+++ src/Login.js
@@ -9,9 +9,9 @@ class Login extends Component {
                 margin: "0 auto", display: "flex", flexDirection: "column",
                 alignItems: "center", justifyContent: "center"
             }}>
-                <input type="text" value="Username" className="nes-input is-primary" style={{ width: "90%", height: "50px", margin: "10px", textAlign: "center" }}/>  
-                <input type="password" value="********" className="nes-input is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
-                <input type="button" value="Login" className="nes-btn is-primary" style={{ width: "75%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="text" placeholder="Username" className="nes-input is-primary" style={{ width: "100%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="password" placeholder="********" className="nes-input is-primary" style={{ width: "100%", height: "50px", margin: "10px", textAlign: "center" }}/>  
+                <input type="button" value="Login" className="nes-btn is-primary" style={{ width: "100%", height: "50px", margin: "10px", textAlign: "center" }}/>  
 
             </div>
         )}}
