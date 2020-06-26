ff3026207361c9064526b48e9b12052a08da2819
Michael Alexander <michaela171@gmail.com>
2020-06-25 21:29:45 -0500

    css changes

--- src/Highscores.js
+++ src/Highscores.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 var $ = require('jquery');
 
 class Highscores extends Component {
-    
+
     constructor() {
         super();
         this.state = {
@@ -16,7 +16,7 @@ class Highscores extends Component {
 
     fetchScoreData = () => {
         return $.get("http://localhost:2020/highscores", response => {
-            console.log (response);
+            console.log(response);
             this.setState(
                 {
                     scoreData: response
@@ -38,37 +38,37 @@ class Highscores extends Component {
         }
 
         const scoreBoardView = this.state.scoreData.map((highscores, index) => (
-            <tr key={index}> 
-                    <td>{highscores.playername}</td>
-                    <td style={{textAlign: "right"}}>{highscores.highscore}</td>
-                </tr>
+            <tr key={index}>
+                <td style={{ paddingRight: '4em', textAlign: 'left' }}>{highscores.playername}</td>
+                <td style={{ textAlign: "right" }}>{highscores.highscore}</td>
+            </tr>
         ));
-        
+
         return <div style={{
             width: "100%", height: "100%",
             margin: "0 auto", display: "flex",
-            alignItems: "center", justifyContent: "center", flexDirection: "column"
+            alignItems: "center", justifyContent: "center", flexDirection: "column", textAlign: "left"
         }}>
-        <div>
-            <h1>High Scores</h1>
+            <div>
+                <h1>High Scores</h1>
+            </div>
+            <br/>
+            <div>
+                <table>
+                    <tr>
+                        <th style={{ color: "red", textAlign: "left" }}>Name</th>
+                        <th style={{ color: "red" }}>Score</th>
+                    </tr>
+                    {scoreBoardView}
+                </table>
+            </div>
+            <br />
+            <br />
+            <footer style={{ position: "fixed", bottom: "20px"}}>                
+                    <Link to="/" className="nes-btn is-primary" style={{ height: "50px", margin: "10px, auto", textAlign: "center", marginRight: '3em'}}>Home</Link>
+                    <Link to="/SelectCategory" className="nes-btn is-secondary" style={{ height: "50px", margin: "10px, auto", textAlign: "center" }}>Play Again!</Link> 
+            </footer>
         </div>
-        <br/>
-        <div>
-            <table>
-                <tr>
-                    <th style={{color: "red"}}>Name</th>
-                    <th style={{color: "red"}}>Score</th>
-                </tr>
-                {scoreBoardView}
-            </table>
-        </div>
-        <br/>
-        <br/>
-        <footer style={{position: "fixed", bottom: "20px"}}>
-            <Link to="/" className="nes-btn is-primary" style={{ height: "50px", margin: "10px, auto", textAlign: "center" }}>Home</Link>
-            <Link to="/SelectCategory" className="nes-btn is-secondary" style={{ height: "50px", margin: "10px, auto", textAlign: "center" }}>Play Again!</Link>
-        </footer>
-    </div>
     }
 }
 
