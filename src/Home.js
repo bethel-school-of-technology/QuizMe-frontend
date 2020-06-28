4b0a176b6c99c24386453432bd0a79242689de61
Sean Combs <seanmxd@gmail.com>
2020-06-28 19:44:02 -0400

    Made letters cycle RTL

--- src/Home.js
+++ src/Home.js
@@ -19,13 +19,13 @@ class Home extends Component {
       if(!this.cycling) {this.cycleColors(); this.cycling = true}
       return ( 
       [
-              <h1 key={0} id="quizme" style={{textAlign: "center", fontSize: "8em"}}>
-                <span style={{color: this.state.colors[0]}}>Q</span>
-                <span style={{color: this.state.colors[1]}}>u</span>
-                <span style={{color: this.state.colors[2]}}>i</span>
-                <span style={{color: this.state.colors[3]}}>z</span>
-                <span style={{color: this.state.colors[4]}}>M</span>
-                <span style={{color: this.state.colors[5]}}>e</span>
+              <h1 key={0} id="quizme" style={{textAlign: "center", fontSize: "8em", maxWidth: "75vw"}}>
+                <span style={{color: this.state.colors[5]}}>Q</span>
+                <span style={{color: this.state.colors[4]}}>u</span>
+                <span style={{color: this.state.colors[3]}}>i</span>
+                <span style={{color: this.state.colors[2]}}>z</span>
+                <span style={{color: this.state.colors[1]}}>M</span>
+                <span style={{color: this.state.colors[0]}}>e</span>!
               </h1>,
               <Link key={1} to="/SelectCategory" className="nes-btn is-primary" style={{ width: "100%", height: "50px"}}>Play</Link>,
               <Link key={2} to="/Highscores" className="nes-btn is-primary" style={{ width: "100%", height: "50px"}}>Scores</Link>
