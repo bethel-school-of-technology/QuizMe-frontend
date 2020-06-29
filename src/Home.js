d3338c725500b683cd3e31e330ea701d1df5b0af
Sean Combs <seanmxd@gmail.com>
2020-06-28 20:16:50 -0400

    Doesn't look good on super wide screens

--- src/Home.js
+++ src/Home.js
@@ -18,18 +18,18 @@ class Home extends Component {
   render() {
       if(!this.cycling) {this.cycleColors(); this.cycling = true}
       return ( 
-      [
-              <h1 key={0} id="quizme" style={{textAlign: "center", fontSize: "8em", maxWidth: "75vw"}}>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+              <span style={{fontSize: "10vw"}}>
                 <span style={{color: this.state.colors[5]}}>Q</span>
                 <span style={{color: this.state.colors[4]}}>u</span>
                 <span style={{color: this.state.colors[3]}}>i</span>
                 <span style={{color: this.state.colors[2]}}>z</span>
                 <span style={{color: this.state.colors[1]}}>M</span>
                 <span style={{color: this.state.colors[0]}}>e</span>!
-              </h1>,
-              <Link key={1} to="/SelectCategory" className="nes-btn is-primary" style={{ width: "100%", height: "50px"}}>Play</Link>,
-              <Link key={2} to="/Highscores" className="nes-btn is-primary" style={{ width: "100%", height: "50px"}}>Scores</Link>
-      ])
+              </span>
+              <Link to="/SelectCategory" className="nes-btn is-primary" style={{ fontSize: "5vw", width: "40vw", marginTop: "2vw"}}>Play</Link>
+              <Link to="/Highscores" className="nes-btn is-primary" style={{ fontSize: "5vw", width: "40vw", marginTop: "2vw"}}>Scores</Link>
+      </div>)
   }
 
   async cycleColors() {
