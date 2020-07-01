152072ddd37c48fd4facee7f8f66b44b801cf743
Sean Combs <seanmxd@gmail.com>
2020-06-30 23:06:53 -0400

    Setup logout route styling

--- src/Logout.js
+++ src/Logout.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Cookies from 'universal-cookie';
+import './Logout.css';
 const cookies = new Cookies();
 
 class Logout extends Component {
   render() {
     cookies.remove("jwt");
     return (
-      <div style={{ fontFamily: "optima" }} className="m-4">
-        <h3>You Are Now Logged Out</h3>
-        <Link to="/Login" className="nes-btn is-primary quizme-logout-button ">
-          Log Back In!
+      <div id="quizme-logout-container">
+        <h3 style={{textAlign: "center"}}>You Are Now Logged Out</h3>
+        <Link to="/" className="nes-btn is-primary">
+          Home
         </Link>
       </div>
     );
